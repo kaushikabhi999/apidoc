1cb35821bd1217b076c76cde22ecf157a021405d
Amit Kaushik <kaushikabhi999@gmail.com>
2020-10-01 13:09:57 +0530

    Login API added in doc

--- api_data.js
+++ api_data.js
@@ -1,6 +1,81 @@
 define({ "api": [
   {
     "type": "post",
+    "url": "/auth/login",
+    "title": "Application Login",
+    "header": {
+      "fields": {
+        "Header": [
+          {
+            "group": "Header",
+            "type": "String",
+            "optional": false,
+            "field": "Device-Type",
+            "description": "<p>Device Type ios/android.</p>"
+          },
+          {
+            "group": "Header",
+            "type": "String",
+            "optional": false,
+            "field": "App-Version",
+            "description": "<p>Version Code 1.0.0.</p>"
+          },
+          {
+            "group": "Header",
+            "type": "String",
+            "optional": false,
+            "field": "Accept-Language",
+            "description": "<p>Language Code en OR ar.</p>"
+          }
+        ]
+      }
+    },
+    "version": "1.0.0",
+    "name": "Login",
+    "group": "Auth",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "email",
+            "description": "<p>Email.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "password",
+            "description": "<p>Password.</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\n{\n    \"data\": {\n        \"user\": {\n            \"id\": 1,\n            \"uid\": fa3e3c91-df2e-477e-8e59-03d679688ff5,\n            \"name\": John Doe,\n            \"phone\": \"90******99\",\n            \"email\": \"john@doe.com\",\n            \"email_verified_at\": null,\n            \"device_type\": \"android\",\n            \"timestamp\": 1597674522004,\n            \"created_at\": \"2020-08-17 19:58:42\",\n            \"updated_at\": \"2020-08-17 19:58:42\"\n        },\n        \"token\": \"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9eyJhdWQi..............E4MzgsIm5iZiI6MTYwMDE2MTgzOCwiZXhwIjoxNjMxNjk3ODM4LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.\"\n    }\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "examples": [
+        {
+          "title": "Error-Response:",
+          "content": "HTTP/1.1 401 Unauthorized\n{\n  \"message\": \"Invalid Credentials\",\n}\n\nHTTP/1.1 400 Bad Request\n {\n     \"data\": {\n         \"email\": [\n             \"The email field is required.\"\n         ]\n     }\n }",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "../server/app/Http/Controllers/Auth/LoginController.php",
+    "groupTitle": "Auth"
+  },
+  {
+    "type": "post",
     "url": "/api/register",
     "title": "Register",
     "header": {
--- api_project.js
+++ api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-10-01T07:37:47.640Z",
+    "time": "2020-10-01T07:39:36.228Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
