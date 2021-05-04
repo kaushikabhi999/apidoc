1204747e06c8c5c653fdc4223dd0fa666f91d8d7
Amit Kaushik <kaushikabhi999@gmail.com>
2021-05-04 18:15:29 +0530

    updated document list API

--- api_project.js
+++ api_project.js
@@ -1,13 +1,13 @@
 define({
-  "name": "Motor Yard",
+  "name": "tawqi3i",
   "version": "1.0.0",
-  "description": "Car sell and purchase Platform",
+  "description": "The Digital Signing Platform",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-10-01T07:44:42.443Z",
+    "time": "2021-05-04T12:44:24.241Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
