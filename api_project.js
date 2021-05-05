62f271a7b0d8fb5a4512fa6aa9f7c1465b1b7076
Amit Kaushik <kaushikabhi999@gmail.com>
2021-05-05 18:45:42 +0530

    Notifications table implemented

--- api_project.js
+++ api_project.js
@@ -1,13 +1,13 @@
 define({
   "name": "tawqi3i",
-  "version": "1.0.0",
+  "version": "2.0.0",
   "description": "The Digital Signing Platform",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-05-04T12:44:24.241Z",
+    "time": "2021-05-05T13:08:00.273Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
