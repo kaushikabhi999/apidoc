341a5c4900b58d671b30f492b84b66de128622cd
Amit Kaushik <kaushikabhi999@gmail.com>
2021-05-11 16:20:21 +0530

    Added Change Password API

--- api_project.js
+++ api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-05-07T12:05:06.665Z",
+    "time": "2021-05-11T10:46:59.365Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
